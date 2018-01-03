b31bdf61a13c93eca9184d7cdbc75fc4bd40ac69
kevinkace <kevinacameron@hotmail.com>
2018-01-03 10:06:49 -0800

    Fix schema edit vnode ref

--- src/pages/schema-edit/editor.js
+++ src/pages/schema-edit/editor.js
@@ -9,49 +9,48 @@ import "codemirror/addon/edit/closebrackets";
 import "codemirror/addon/selection/active-line";
 import "codemirror/addon/comment/continuecomment";
 
-export function view(vnode) {
-    return m("textarea", {
-            oncreate : function(vnode) {
-                vnode.state.editor = editor.fromTextArea(vnode.dom, {
-                    mode : "application/javascript",
-                    lint : true,
+export function oncreate(vnode) {
+    // Set up codemirror
+    vnode.state.editor = editor.fromTextArea(vnode.dom, {
+        mode : "application/javascript",
+        lint : true,
 
-                    indentUnit   : 4,
-                    smartIndent  : false,
-                    lineNumbers  : true,
-                    lineWrapping : true,
+        indentUnit   : 4,
+        smartIndent  : false,
+        lineNumbers  : true,
+        lineWrapping : true,
 
-                    // Plugin options
-                    styleActiveLine  : true,
-                    continueComments : true,
+        // Plugin options
+        styleActiveLine  : true,
+        continueComments : true,
 
-                    autoCloseBrackets : true,
-                    matchBrackets     : true,
+        autoCloseBrackets : true,
+        matchBrackets     : true,
 
-                    extraKeys : {
-                        Tab : function(cm) {
-                            if(cm.somethingSelected()) {
-                                return cm.indentSelection("add");
-                            }
+        extraKeys : {
+            Tab : function(cm) {
+                if(cm.somethingSelected()) {
+                    return cm.indentSelection("add");
+                }
 
-                            return cm.execCommand(cm.options.indentWithTabs ? "insertTab" : "insertSoftTab");
-                        },
+                return cm.execCommand(cm.options.indentWithTabs ? "insertTab" : "insertSoftTab");
+            },
 
-                        "Shift-Tab" : function(cm) {
-                            cm.indentSelection("subtract");
-                        }
-                    }
-                });
-
-                vnode.state.editor.on("changes", debounce(function() {
-                    var text = vnode.state.editor.doc.getValue();
-
-                    vnode.attrs.ref.child("source").set(text);
-
-                    vnode.attrs.worker.postMessage(text);
-                }, 500, { maxWait : 5000 }));
+            "Shift-Tab" : function(cm) {
+                cm.indentSelection("subtract");
             }
-        },
-        vnode.attrs.source
-    );
+        }
+    });
+
+    vnode.state.editor.on("changes", debounce(function() {
+        var text = vnode.state.editor.doc.getValue();
+
+        vnode.attrs.ref.child("source").set(text);
+
+        vnode.attrs.worker.postMessage(text);
+    }, 500, { maxWait : 5000 }));
+}
+
+export function view(vnode) {
+    return m("textarea", vnode.attrs.source);
 }
