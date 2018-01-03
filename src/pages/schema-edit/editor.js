cbe9cb0315d131c589a1d5496400f625713639e6
kevinkace <kevinacameron@hotmail.com>
2018-01-03 09:55:22 -0800

    Fix codemirror initialization

--- src/pages/schema-edit/editor.js
+++ src/pages/schema-edit/editor.js
@@ -9,56 +9,49 @@ import "codemirror/addon/edit/closebrackets";
 import "codemirror/addon/selection/active-line";
 import "codemirror/addon/comment/continuecomment";
 
-export function oninit(vnode) {
-    // Set up codemirror
-    vnode.state.editorSetup = function(el, init) {
-        if(init) {
-            return;
-        }
-
-        vnode.state.editor = editor.fromTextArea(el, {
-            mode : "application/javascript",
-            lint : true,
-
-            indentUnit   : 4,
-            smartIndent  : false,
-            lineNumbers  : true,
-            lineWrapping : true,
-
-            // Plugin options
-            styleActiveLine  : true,
-            continueComments : true,
-
-            autoCloseBrackets : true,
-            matchBrackets     : true,
-
-            extraKeys : {
-                Tab : function(cm) {
-                    if(cm.somethingSelected()) {
-                        return cm.indentSelection("add");
-                    }
-
-                    return cm.execCommand(cm.options.indentWithTabs ? "insertTab" : "insertSoftTab");
-                },
-
-                "Shift-Tab" : function(cm) {
-                    cm.indentSelection("subtract");
-                }
-            }
-        });
-
-        vnode.state.editor.on("changes", debounce(function() {
-            var text = vnode.state.editor.doc.getValue();
-
-            vnode.attrs.ref.child("source").set(text);
-            
-            vnode.attrs.worker.postMessage(text);
-        }, 500, { maxWait : 5000 }));
-    };
-}
-
 export function view(vnode) {
-    return m("textarea", { config : vnode.state.editorSetup },
+    return m("textarea", {
+            oncreate : function(vnode) {
+                vnode.state.editor = editor.fromTextArea(vnode.dom, {
+                    mode : "application/javascript",
+                    lint : true,
+
+                    indentUnit   : 4,
+                    smartIndent  : false,
+                    lineNumbers  : true,
+                    lineWrapping : true,
+
+                    // Plugin options
+                    styleActiveLine  : true,
+                    continueComments : true,
+
+                    autoCloseBrackets : true,
+                    matchBrackets     : true,
+
+                    extraKeys : {
+                        Tab : function(cm) {
+                            if(cm.somethingSelected()) {
+                                return cm.indentSelection("add");
+                            }
+
+                            return cm.execCommand(cm.options.indentWithTabs ? "insertTab" : "insertSoftTab");
+                        },
+
+                        "Shift-Tab" : function(cm) {
+                            cm.indentSelection("subtract");
+                        }
+                    }
+                });
+
+                vnode.state.editor.on("changes", debounce(function() {
+                    var text = vnode.state.editor.doc.getValue();
+
+                    vnode.attrs.ref.child("source").set(text);
+
+                    vnode.attrs.worker.postMessage(text);
+                }, 500, { maxWait : 5000 }));
+            }
+        },
         vnode.attrs.source
     );
 }
